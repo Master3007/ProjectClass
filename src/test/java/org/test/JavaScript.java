2fab195a5e703e18b0927fe1cb1da272838b909f
Master3007 <gbmathiazhagan78@gmail.com>
2025-05-23 20:48:03 +0530

    New Code

---
+++ src/test/java/org/test/JavaScript.java
@@ -0,0 +1,31 @@
+package org.test;
+
+import java.time.Duration;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class JavaScript {
+public static void main(String[] args) {
+	WebDriver driver=new ChromeDriver();
+	driver.get("https://www.facebook.com/");
+	driver.manage().window().maximize();
+	driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(20));
+
+	JavascriptExecutor js= (JavascriptExecutor) driver;
+	
+	WebElement Txtuser = driver.findElement(By.id("email"));
+	js.executeScript("arguments[0].setAtrribute('Value', 'Mathi')", Txtuser);
+
+	Object executeScript = js.executeScript("return aruguments[0].getAttribute('value')", Txtuser);
+	System.out.println(executeScript);
+
+	WebElement textPass = driver.findElement(By.id("pass"));
+	js.executeScript("arguments[0].setAttribute('value','1587')", textPass);
+
+
+}
+}
